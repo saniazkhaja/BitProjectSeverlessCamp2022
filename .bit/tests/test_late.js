a8e83d9368b4b65382a4708befe5c31225f4722f
Sania Khaja <49351072+saniazkhaja@users.noreply.github.com>
2022-04-21 10:24:08 -0500

    Update test_late.js

--- .bit/tests/test_late.js
+++ .bit/tests/test_late.js
@@ -6,8 +6,8 @@ const repo = args['repo'];
 async function main() {
     try { rl = require('./../../late.js') }
     catch (e) {
-        console.log("Searching for 'bubble_sort.js'... file cannot be found");
-        await functions.throwError("Searching for 'bubble_sort.js'... file cannot be found", user, repo)
+        console.log("Searching for 'late.js'... file cannot be found");
+        await functions.throwError("Searching for 'late.js'... file cannot be found", user, repo)
 
         process.exit(1)
     }
@@ -20,8 +20,8 @@ async function main() {
         userOutput = rl.running_late(testInput);
     }
     catch (e) {
-        console.log("Searching for function 'bubble_sort()'... function cannot be found");
-        await functions.throwError("Searching for function 'bubble_sort()'... function cannot be found", user, repo)
+        console.log(e);
+        await functions.throwError(e, user, repo)
         process.exit(1);
     }
 
@@ -40,8 +40,8 @@ async function main() {
         userOutput = rl.running_late(testInput);
     }
     catch (e) {
-        console.log("Searching for function 'running_late()'... function cannot be found");
-        await functions.throwError("Searching for function 'running_late()'... function cannot be found", user, repo)
+        console.log(e);
+        await functions.throwError(e, user, repo)
         process.exit(1);
     }
 
