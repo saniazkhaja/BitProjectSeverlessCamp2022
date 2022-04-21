fead6ab31bbd2ce45e1fd87c8572abdeb9bb968c
Sania <saniazkhaja@gmail.com>
2022-04-21 10:59:31 -0500

    Finding max value in array

--- max_nums.js
+++ max_nums.js
@@ -1,1 +1,12 @@
 // start coding your function here!
+function find_max(arr) {
+    let max = arr[0];
+
+    for (let i = 0; i < arr.length; i++) {
+        if (max < arr[i]) {
+            max = arr[i];
+        }
+    }
+}
+
+exports.find_max = find_max;
