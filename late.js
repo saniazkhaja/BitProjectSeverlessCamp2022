ab9b1dd14308d8730f443ebbcc9fc0f9840943e7
Sania <saniazkhaja@gmail.com>
2022-04-21 09:25:47 -0500

    checking time and displays message

--- late.js
+++ late.js
@@ -2,11 +2,11 @@
 
 function running_late(date) {
     let time = date.substr(text.length - 8, 8);
-    if (date < "10:00:00") {
+    if (time < "10:00:00") {
         return "It is still early";
     } else {
         return "It is late!";
     }
 }
 
-module.exports = running_late
+module.exports = running_late;
